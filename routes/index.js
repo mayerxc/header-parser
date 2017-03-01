5f7ff11a0edb3a4a35cf7f9ba4c3c08504d5205f
Chris Mayer <mayerxc@gmail.com>
2017-02-28 19:58:23 -0500

    updated the index file and then pointed it to a json route also

--- routes/index.js
+++ routes/index.js
@@ -4,45 +4,43 @@ var app = express();
 var useragent = require('express-useragent');
 var accepts = require('accepts');
 
-app.use( useragent.express() );
+app.use(useragent.express());
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-
+router.get('/', function (req, res, next) {
   //var ip = req.ip;
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var source = req.headers['user-agent'];
   var languageHeaders = req.headers['accept-language'];
   var languageAccepts = accepts(req).languages();
   var language = languageAccepts[0];
-  var jsonData = JSON.stringify( useragent.parse(source));
+  var jsonData = JSON.stringify(useragent.parse(source));
   var ua = useragent.parse(source);
   var os = ua.os;
   //var ip = ua.geoIp;//won't exist until I put up on heroku
   var browser = ua.browser;
   var browserVersion = ua.version;
-  
-  
-  console.log("ip address is: "+ ip );
+
+
+  console.log("ip address is: " + ip);
   console.log("req.headers['user-agent'] is: " + source);
   console.log("req.useragent is: " + jsonData);
-  console.log("os is: "+ os);
-  console.log('languageHeaders is: '+ languageHeaders);
+  console.log("os is: " + os);
+  console.log('languageHeaders is: ' + languageHeaders);
   console.log("languageAccepts from accepts is: " + languageAccepts);
   console.log('index[0] of languageAccepts is: ' + languageAccepts[0]);
   console.log('ua is: ' + ua);
-  
-/*  res.json( {
-    "ipaddress": ip,
-    "language": language,
-    "software": os,
-    "browser":browser,
-    "browser version":browserVersion
-  });*/
+
+  /*  res.json( {
+      "ipaddress": ip,
+      "language": language,
+      "software": os,
+      "browser":browser,
+      "browser version":browserVersion
+    });*/
 
 
-  res.render('index', { 
+  res.render('index', {
     useragent: jsonData,
     ip: ip,
     source: source,
@@ -51,8 +49,43 @@ router.get('/', function(req, res, next) {
     "browserVersion": browserVersion,
     language: language,
     languageAccepts: languageAccepts
-   });
-  
+  });
+
 });
+// Respond with json file
+
+router.get('/json', function (req, res, next) {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var source = req.headers['user-agent'];
+    var languageHeaders = req.headers['accept-language'];
+    var languageAccepts = accepts(req).languages();
+    var language = languageAccepts[0];
+    var jsonData = JSON.stringify(useragent.parse(source));
+    var ua = useragent.parse(source);
+    var os = ua.os;
+    //var ip = ua.geoIp;//won't exist until I put up on heroku
+    var browser = ua.browser;
+    var browserVersion = ua.version;
+
+
+    console.log("ip address is: " + ip);
+    console.log("req.headers['user-agent'] is: " + source);
+    console.log("req.useragent is: " + jsonData);
+    console.log("os is: " + os);
+    console.log('languageHeaders is: ' + languageHeaders);
+    console.log("languageAccepts from accepts is: " + languageAccepts);
+    console.log('index[0] of languageAccepts is: ' + languageAccepts[0]);
+    console.log('ua is: ' + ua);
+    console.log("json is the router!");
+    
+    //res with json data
+    res.json({
+      "ipaddress": ip,
+      "language": language,
+      "software": os,
+      "browser": browser,
+      "browser version": browserVersion
+    });
+  });
 
 module.exports = router;
